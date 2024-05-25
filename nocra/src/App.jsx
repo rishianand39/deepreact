753a47f052e8e1abda4610f078b27cbe0e4efe7f
rishianand39 <rishi.rn818@gmail.com>
2024-05-25 22:51:56 +0530

    react app initalized without any npx create react-app

---
+++ nocra/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./App.css"
+
+const App = () => {
+  return (
+    <div id='app'>React app</div>
+  )
+}
+
+export default App
--- nocra/webpack.config.js
+++ nocra/webpack.config.js
@@ -1,28 +1,40 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"], // add .jsx to the list of extensions to resolve
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
     },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    },
-    devServer: {
-        contentBase: './dist',
-        port: 3000
-    }
+    compress: true,
+    port: 3001,
+  },
 };
