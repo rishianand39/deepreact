a46feaa7d323a479247873750f787befb3254828
rishianand39 <rishi.rn818@gmail.com>
2024-05-26 15:38:30 +0530

    react image

--- nocra/src/App.jsx
+++ nocra/src/App.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import "./App.css"
+import React from "react";
+import "./App.css";
+import ImgLoader from "./ImgLoader";
+import {Img} from 'react-image'
 
 const App = () => {
   return (
-    <div id='app'>React app</div>
-  )
-}
+    <div id="app">
+      <Img
+        src={[
+          "https://images.pexels.com/photos/1402787/pexels-photo-1402787.jpeg?auto=compress&cs=tinysrgb&w=600",
+        ]}
+        loader={<ImgLoader />}
+      />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ nocra/src/ImgLoader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ImgLoader = () => {
+  return (
+    <h1>ImgLoader</h1>
+  )
+}
+
+export default ImgLoader
